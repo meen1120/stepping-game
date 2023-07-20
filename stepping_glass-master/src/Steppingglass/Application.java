25e0be6d0ef34c2118627c2bddbc27a8aa1743ba
tinypig705 <tinypig705@naver.com>
2023-07-20 19:03:47 +0900

    오징어게임

---
+++ stepping_glass-master/src/Steppingglass/Application.java
@@ -0,0 +1,12 @@
+package Steppingglass;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Start st = new Start();
+        st.start();
+    }
+}
---
+++ stepping_glass-master/src/Steppingglass/RandomNumber.java
@@ -0,0 +1,9 @@
+package Steppingglass;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public interface RandomNumber {
+
+    public void randomnum();
+}
